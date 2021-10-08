4b7fd1823f61523b89dec7dfa0619d8328d2dd7c
Kasper Isager Dalsgarð <kasperisager@hey.com>
2021-10-08 13:02:40 +0200

    Infer type declarations

---
+++ types/index.d.ts
@@ -0,0 +1,9 @@
+declare const _exports: any;
+export = _exports;
+export var skip: any;
+export var todo: any;
+export var configure: any;
+export { exports as test };
+declare namespace exports {
+    export { skip, todo, configure, exports as test };
+}
