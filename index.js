af0db87144e734f7bf0121e7edf8b63ecd16b848
utanapishtim <>
2022-01-11 08:20:03 -0800

    fixes issue where brittle exists becuase existing uncaught exception capture callback

--- index.js
+++ index.js
@@ -51,6 +51,9 @@ const SOLO = Number.isInteger(+env.SOLO) ? !!env.SOLO : env.SOLO && new RegExp(e
 
 Object.hasOwn = Object.hasOwn || ((o, p) => Object.hasOwnProperty.call(o, p))
 
+// if the process has an uncaught exception capture callback set, blow it away and set our own
+if (process.hasUncaughtExceptionCaptureCallback()) process.setUncaughtExceptionCaptureCallback(null)
+
 process.setUncaughtExceptionCaptureCallback((err) => {
   Object.defineProperty(err, 'fatal', { value: true })
   if (!process.emit('uncaughtException', err)) {
