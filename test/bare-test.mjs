56d6fef8caf39192c8aad7bf9c22652fa90b412e
Tobias Baunbaek <freeall@gmail.com>
2024-02-02 15:38:33 +0100

    exitCode should work for node

--- test/bare-test.mjs
+++ test/bare-test.mjs
@@ -3,20 +3,23 @@ import { IS_BARE } from '../lib/constants.js'
 
 let runtime
 let spawn
-if (IS_BARE) {
-  const subprocess = await import('bare-subprocess')
-  runtime = 'bare'
-  spawn = subprocess.spawn
-} else {
-  const cp = await import('child_process')
-  runtime = 'node'
-  spawn = cp.spawn
-}
+
+test('setup', async t => {
+  if (IS_BARE) {
+    const subprocess = await import('bare-subprocess')
+    runtime = 'bare'
+    spawn = subprocess.spawn
+  } else {
+    const cp = await import('child_process')
+    runtime = 'node'
+    spawn = cp.spawn
+  }
+})
 
 test('running bare script fails as intended', async t => {
   t.plan(4)
   const { exitCode, error, stdout, stderr } = await executeCode('./test/helpers/bare-test-script.js')
-  t.is(exitCode, 0) // 1 for 'node, 0 for 'bare'
+  t.is(exitCode, 1) // 1 for 'node, 0 for 'bare'
   t.absent(error)
   t.ok(stderr.includes('assertion count (0) did not reach plan (1)'))
   t.ok(stdout.includes('test should fail')) // Name of the test, stored in ./helpers/bare-test-script.js
