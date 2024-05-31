c38569bb039c74eb622518ff28ff4a24fbeef425
Mathias Buus <mathiasbuus@gmail.com>
2024-05-31 10:42:49 +0200

    add event flush helper

--- index.js
+++ index.js
@@ -280,6 +280,10 @@ class Test {
     return this._hasPlan && this._planned === 0
   }
 
+  eventFlush () {
+    return eventFlush()
+  }
+
   then (...args) {
     return this.promise.then(...args)
   }
@@ -704,6 +708,7 @@ exports.resume = resume
 
 // Used by snapshots
 exports.createTypedArray = createTypedArray
+exports.eventFlush = eventFlush
 
 function configure ({ timeout = DEFAULT_TIMEOUT, bail = false, solo = false, source = true } = {}) {
   const runner = getRunner()
@@ -846,4 +851,8 @@ function prematureEnd (t, message) {
     : ''
 
   return new Error(message + details)
+}
+
+function eventFlush () {
+  return new Promise(resolve => setImmediate(resolve))
 }
