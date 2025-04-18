a7eb42afa4128e52cb890a29f45d741324110e13
Jan Keith Darunday <jkcdarunday@gmail.com>
2025-04-18 20:24:43 +0800

    Add test for commandless coverage

---
+++ test/commandless-cov.mjs
@@ -0,0 +1,34 @@
+import { spawner } from './helpers/index.js'
+
+await spawner(
+  function ({ configure, test }) {
+    configure({ coverage: true })
+
+    test('nested stealth bails on first failure', function (t) {
+      t.pass('prints here')
+      t.pass('also prints here')
+    })
+  },
+  `
+  TAP version 13
+
+  # nested stealth bails on first failure
+      ok 1 - prints here
+      ok 2 - also prints here
+  ok 1 - nested stealth bails on first failure # time = 0.295115ms
+
+  1..1
+  # tests = 1/1 pass
+  # asserts = 2/2 pass
+  # time = 18.422843ms
+
+  # ok
+  ----------|---------|----------|---------|---------|------------------------------------------------
+  File      | % Stmts | % Branch | % Funcs | % Lines | Uncovered Line #s                              
+  ----------|---------|----------|---------|---------|------------------------------------------------
+  All files |   93.44 |    47.05 |   96.29 |   93.44 |                                                
+   index.js |   93.44 |    47.05 |   96.29 |   93.44 | ...409-410,618-619,634-635,641-647,684-686,825 
+  ----------|---------|----------|---------|---------|------------------------------------------------
+  `,
+  { exitCode: 0, stderr: '' }
+)
