6015ca2aeca036a4af02a4cf9a27986d18770455
Jan Keith Darunday <jkcdarunday@gmail.com>
2025-04-25 04:17:43 +0800

    Move status extraction to its own line

--- bin/brittle-bare.js
+++ bin/brittle-bare.js
@@ -1,11 +1,8 @@
 #!/usr/bin/env bare
 
 if (!global.Bare && global.process) {
-  process.exit(
-    require('child_process')
-      .spawnSync('bare', process.argv.slice(1), { stdio: 'inherit', shell: true })
-      .status
-  )
+  const { status } = require('child_process').spawnSync('bare', process.argv.slice(1), { stdio: 'inherit', shell: true })
+  process.exit(status)
 }
 
 require('../cmd.js')
