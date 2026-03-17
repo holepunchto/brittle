9fb325d51b99f68c275fbea613a791bca21a85ec
davidmarkclements <david.mark.clements@gmail.com>
2026-03-17 19:36:56 +0100

    win -> node -> spawn bare

--- brittle-bare.js
+++ brittle-bare.js
@@ -1,2 +1,10 @@
 #!/usr/bin/env bare
+const { isNode, isWindows } = require('which-runtime')
+const { spawnSync } = require('child_process')
+if (isNode) {
+  const ext = isWindows ? '.exe' : ''
+  const { status } = spawnSync('bare' + ext, process.argv.slice(1), { stdio: 'inherit' })
+  process.exit(status)
+}
 require('./cmd.js')
+
