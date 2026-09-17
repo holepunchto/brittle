621b4c3ae430e50ec33925085c5d6105d1b04a59
Jan Keith Darunday <jkcdarunday@gmail.com>
2026-09-17 21:16:48 +0800

    Increase timeout of never resolve test

--- test/no-handles-unending.mjs
+++ test/no-handles-unending.mjs
@@ -2,7 +2,7 @@ import { spawner } from './helpers/index.js'
 
 await spawner(
   function (test) {
-    test('never resolve', { timeout: 100 }, async function (t) {
+    test('never resolve', { timeout: 500 }, async function (t) {
       await new Promise((resolve) => {})
     })
   },
